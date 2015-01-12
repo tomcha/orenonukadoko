4f514803787c9aaf5a0512bde5d933a934ac38be
tomcha <tomcha@tomcha.net>
2015-01-12 21:51:57 +0900

    gemファイル追加など初期設定

--- Gemfile
+++ Gemfile
@@ -1,4 +1,10 @@
 source "https://rubygems.org"
 gem 'sinatra'
 gem 'haml'
+gem 'omniauth'
+gem 'omniauth-twitter'
+
+gem 'activerecord'
+gem 'sqlite3'
+
 gem 'rspec'
--- app/orenonukadoko.rb
+++ app/orenonukadoko.rb
@@ -1,5 +1,9 @@
 require 'sinatra/base'
 require 'haml'
+require 'omniauth'
+require 'omniauth-twitter'
+require 'activerecord'
+require 'sqlite3'
 
 class Orenonukadoko< Sinatra::Base
   get '/' do
