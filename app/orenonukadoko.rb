a8088c7f6fd95c725d3abafdaf1ea13a1ba99fd9
tomcha <tomcha@tomcha.net>
2015-01-14 18:42:36 +0900

    twitter oauth認証用キーの読み込みを追加

--- app/orenonukadoko.rb
+++ app/orenonukadoko.rb
@@ -2,10 +2,30 @@ require 'sinatra/base'
 require 'haml'
 require 'omniauth'
 require 'omniauth-twitter'
-require 'activerecord'
+require 'active_record'
 require 'sqlite3'
 
 class Orenonukadoko< Sinatra::Base
+
+  configure do
+    set :public_folder, File.expand_path(File.join(root, '..', 'public'))
+    enable :sessions
+
+    twitter_config = Hash.new
+    File.open(File.expand_path(File.join(root, '..', 'config', 'twitter_config.txt'))) do |file|
+      file.each_line do |text|
+        (key,value) = text.split(":")
+        twitter_config[key] = value
+      end
+      twitter_config['access_token'] = ''
+      twitter_config['access_token_secret'] = ''
+    end
+
+    use OmniAuth::Builder do
+      provider :twitter, twitter_config['consumer_key'], twitter_config['consumer_secret']
+    end
+  end
+
   get '/' do
     haml :index
   end
